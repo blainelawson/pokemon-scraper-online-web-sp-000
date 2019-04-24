7e8b70c9b5c7f7ac5ec1930609172a94e59390cc
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-24 13:16:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Pokemon
+<<<<<<< HEAD
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
 
@@ -43,5 +44,9 @@ class Pokemon
     db.execute(sql, hp, self.id)
     # binding.pry
     self.hp = hp
+=======
+
+  def initialize()
+>>>>>>> 5244440d4d88930ae006c229c6da085294272648
   end
 end
